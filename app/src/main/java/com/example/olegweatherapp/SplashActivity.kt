23c98ad9b1d7fb1f87781454e23002e14b453db2
DragunOleg <dragun.oleg41@gmail.com>
2021-04-12 13:10:12 +0300

    Basic splash

---
+++ app/src/main/java/com/example/olegweatherapp/SplashActivity.kt
@@ -0,0 +1,30 @@
+package com.example.olegweatherapp;
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.*
+
+class SplashActivity : AppCompatActivity() {
+
+    private val activityScope = CoroutineScope(Dispatchers.Main)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        activityScope.launch {
+            delay(3000)
+
+            var intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    override fun onPause() {
+        activityScope.cancel()
+        super.onPause()
+    }
+
+}
