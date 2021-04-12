b2c6029ef40128910d935650cc45c5aac16ce6dc
DragunOleg <dragun.oleg41@gmail.com>
2021-04-12 14:54:52 +0300

    splash screen done

--- app/src/main/java/com/example/olegweatherapp/SplashActivity.kt
+++ app/src/main/java/com/example/olegweatherapp/SplashActivity.kt
@@ -3,7 +3,10 @@ package com.example.olegweatherapp;
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.coroutines.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashActivity : AppCompatActivity() {
 
@@ -12,19 +15,12 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-
         activityScope.launch {
-            delay(3000)
-
-            var intent = Intent(this@SplashActivity, MainActivity::class.java)
-            startActivity(intent)
+            delay(1720)
+            startActivity(Intent(
+                    this@SplashActivity,
+                    MainActivity::class.java))
             finish()
         }
     }
-
-    override fun onPause() {
-        activityScope.cancel()
-        super.onPause()
-    }
-
 }
